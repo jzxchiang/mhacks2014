3f1ba5f2f1ad931d69ad3ae14128a5d8c4f49b7d
Justin Chiang <jzx.chiang@gmail.com>
2014-09-06 18:43:48 -0400

    Add form validation for empty <input> elements

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -10,10 +10,21 @@ angular.module('core').controller('HomeController', ['$scope', '$http', 'Authent
 			interests: []
 		};
 		$scope.currentStep = 1;
+        
+        $scope.addInterest = function () {
+            if ($scope.tempInterest) {
+                $scope.credentials.interests.push($scope.tempInterest);
+                $scope.tempInterest = '';
+                angular.element(document.querySelector('#input-one')).removeClass('red-placeholder red-border');
+            }
+        };
 
 		$scope.checkInterests = function () {
 			if ($scope.credentials.interests.length !== 0) {
 				$scope.currentStep = 2;
+			} else {
+				angular.element(document.querySelector('#input-one')).attr('placeholder', 'Please enter at least one interest');
+				angular.element(document.querySelector('#input-one')).addClass('red-placeholder red-border');
 			}
 		};
 
@@ -29,15 +40,18 @@ angular.module('core').controller('HomeController', ['$scope', '$http', 'Authent
 					$scope.error = response.message;
 				});
 				$scope.currentStep = 3;
+			} else {
+				if (!$scope.credentials.name) {
+					angular.element(document.querySelector('#input-two')).attr('placeholder', 'Please enter your name');
+					angular.element(document.querySelector('#input-two')).addClass('red-placeholder red-border');
+
+				}
+				if (!$scope.credentials.email) {
+					angular.element(document.querySelector('#input-three')).attr('placeholder', 'Please enter a valid email');
+					angular.element(document.querySelector('#input-three')).addClass('red-placeholder red-border');
+				}
 			}
 		};
-        
-        $scope.addInterest = function () {
-            if ($scope.tempInterest) {
-                $scope.credentials.interests.push($scope.tempInterest);
-                $scope.tempInterest = '';
-            }
-        };
 
         $scope.validateColumbiaEmail = function () {
 
